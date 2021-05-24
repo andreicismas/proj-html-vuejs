9d3d7e307aeb7360ebef077eada07763171af107
andreicismas <andrei_cismas@yahoo.it>
2021-05-24 15:44:59 +0200

    add new project

---
+++ script.js
@@ -0,0 +1,43 @@
+new Vue({
+    el: "#root",
+  
+    data: {
+      navLinks:[
+        {
+          name: "HOME",
+          link: "#",
+        },
+        {
+          name: "MISSION",
+          link: "#",
+        },
+        {
+          name: "CAUSES",
+          link: "#",
+        },
+        {
+          name: "JOURNAL",
+          link: "#",
+        },
+        {
+          name: "DONATE",
+          link: "#",
+        },
+
+      ],
+    },
+  
+    computed: {
+  
+    },
+  
+    methods: {
+      
+    },
+  
+    mounted() {
+        
+    },
+  
+  
+  });
